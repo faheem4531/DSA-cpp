8bc7c2063d39e22a3681a3bcd2b79432ed24d0cd
faheem4531 <aa0561085@gmail.com>
2022-11-17 01:21:42 +0500

    Subtraction Matrices functinal.....es

--- matrixOperator.cpp
+++ matrixOperator.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
 using namespace std;
 //*********************************  Functions Handler
-int checkAdding(int **, int **, int **, int, int, int, int);
-// int menueSubtract(int **, int **, int **, int, int, int, int);
+void checkAdding(int **, int **, int **, int, int, int, int);
+int menueSubtract(int **, int **, int **, int, int, int, int);
 //*********************************   Logical Functions Controler
 void SetMatrix(int **, int, int);
 void ShowMatrix(int **, int, int);
 void add(int **, int **, int, int, int, int);
+void subtract(int **, int **, int **, int, int, int, int);
 //*******************************   Main
 int main()
 {
@@ -40,12 +41,14 @@ int main()
   while (1)
   {
     system("cls");
+
     cout << "1. Set Matrix-1" << endl
          << "2. Set Matrix-2" << endl
          << "3. Show Matrix-1" << endl
          << "4. Show Matrix-2" << endl
          << "5. Adding" << endl
-         << "6. Exit" << endl;
+         << "6. Subtraction" << endl
+         << "7. Exit" << endl;
     cout << "\nEnter Choice \t";
     cin >> hit;
     cout << endl;
@@ -65,7 +68,11 @@ int main()
       break;
     case (5):
       checkAdding(matrix1, matrix2, apply, r1, r2, c1, c2);
+      break;
     case (6):
+      menueSubtract(matrix1, matrix2, apply, r1, r2, c1, c2);
+      break;
+    case (7):
       return 0;
     }
     ch = cin.get();
@@ -100,7 +107,7 @@ void ShowMatrix(int **array, int r, int c) //       Show Matrix
   }
 }
 
-int checkAdding(int **array1, int **array2, int **add, int r1, int r2, int c1, int c2)
+void checkAdding(int **array1, int **array2, int **add, int r1, int r2, int c1, int c2)
 {
   if (r1 == r2 && c1 == c2)
   {
@@ -111,11 +118,60 @@ int checkAdding(int **array1, int **array2, int **add, int r1, int r2, int c1, i
         add[i][j] = array1[i][j] + array2[i][j];
       }
     }
-    cout << "Sum of 2 Matrixes " << endl;
+    cout << "Sum of 2 Matrixes \n"
+         << endl;
     ShowMatrix(add, r1, c1);
   }
   else
     cout << "Matrix Addition Rule Error" << endl;
 
-  return 0;
+  // return 0;
+}
+
+int menueSubtract(int **array1, int **array2, int **sub, int r1, int r2, int c1, int c2)
+{
+  char ch;
+  int choice;
+  while (1)
+  {
+    system("cls");
+    cout << "Subtracting Matrices" << endl;
+    cout << "1. Subtract Matrix 1 from 2 " << endl
+         << "2. Subtract Matrix 2 from 1 " << endl
+         << "3. return" << endl;
+    cout << "Enter your Choice \t";
+    cin >> choice;
+    switch (choice)
+    {
+    case (1):
+      subtract(array1, array2, sub, r1, r2, c1, c2);
+      break;
+    case (2):
+      subtract(array2, array1, sub, r1, r2, c1, c2);
+      break;
+    case (3):
+      return 0;
+    }
+    ch = cin.get();
+    ch = cin.get();
+  }
+}
+
+void subtract(int **array1, int **array2, int **sub, int r1, int r2, int c1, int c2)
+{
+  if (r1 == r2 && c1 == c2)
+  {
+    for (int i = 0; i < r1; i++)
+    {
+      for (int j = 0; j < c1; j++)
+      {
+        sub[i][j] = array1[i][j] - array2[i][j];
+      }
+    }
+    cout << "Subtraction \n"
+         << endl;
+    ShowMatrix(sub, r1, c1);
+  }
+  else
+    cout << "Matrix Subtraction Rule Error" << endl;
 }
