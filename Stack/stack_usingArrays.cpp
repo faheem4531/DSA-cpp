a38ab1efd2a6c227d82c505b8ba8fbb107a848ee
faheem4531 <aa0561085@gmail.com>
2022-12-21 16:59:00 +0500

    pop

---
+++ Stack/stack_usingArrays.cpp
@@ -0,0 +1,105 @@
+#include <iostream>
+using namespace std;
+//                                                       Class
+class Stack
+{
+private:
+  int *arr;
+  int top;
+  int size;
+
+public:
+  Stack(int n)
+  {
+    size = n;
+    arr = new int[size];
+    top = -1;
+  }
+  bool isEmpty()
+  {
+    if (top == -1)
+      return true;
+    else
+      return false;
+  }
+  bool isFull()
+  {
+    if (top == size - 1)
+      return true;
+    else
+      return false;
+  }
+  void push()
+  {
+    if (isFull() == 1) //           top == size - 1
+      cout << "Stack is Full " << endl;
+    else
+    {
+      int value;
+      cout << "Enter the value you want to Push \t";
+      cin >> value;
+      top = top + 1;
+      arr[top] = value;
+      display();
+    }
+  }
+  void pop()
+  {
+    if (isEmpty() == 1)
+      cout << "Stack is Empty" << endl;
+    else
+    {
+      top = top - 1;
+      display();
+    }
+  }
+  void display()
+  {
+    if (isEmpty() == 1)
+    {
+      cout << "stack is Empty" << endl;
+      return;
+    }
+    cout << "\n \t \t Data in your Stack is " << endl;
+    for (int i = 0; i <= top; i++)
+    {
+      cout << arr[i] << "\t";
+    }
+    cout << endl;
+  }
+};
+//                                                        Main
+int main()
+{
+  int size, choice;
+  char ch;
+  cout << "Enter the size for your Stack" << endl;
+  cin >> size;
+  Stack obj(size);
+  while (1)
+  {
+    system("cls");
+    cout << "1. Push" << endl
+         << "2. Pop" << endl
+         << "3. Display" << endl
+         << "4. Exit" << endl;
+    cout << "\n \t Enter your choice \t";
+    cin >> choice;
+    switch (choice)
+    {
+    case 1:
+      obj.push();
+      break;
+    case 2:
+      obj.pop();
+      break;
+    case 3:
+      obj.display();
+      break;
+    case 4:
+      return 0;
+    }
+    ch = cin.get();
+    ch = cin.get();
+  }
+}
