a21315a652b59d6be0192a1d9fe897b99a72fb0a
faheem4531 <aa0561085@gmail.com>
2022-12-24 01:22:54 +0500

    postfix solution code

--- Stack/infix_into_postfix.cpp
+++ Stack/infix_into_postfix.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
+#include "math.h"
 using namespace std;
+//                                                                Global Expression
+string postfix_Expression = "795-2*+";
 //                                                                Class
-class Expr
+class infix_To_Postfix
 {
 private:
-  int top, j;
+  int top;
   char stack[30], ch;
   string expresion;
   string postfix;
 
 public:
-  Expr()
+  infix_To_Postfix()
   {
     top = -1;
     postfix = "";
-    j = 0;
   }
   bool isEmpty()
   {
@@ -103,7 +105,80 @@ public:
   void print()
   {
     cout << "\n \t \t \t Your Postfix Expression is = " << postfix << endl;
-    cout << endl;
+    postfix_Expression = postfix;
+  }
+};
+
+//                                                                Class 2nd
+class expression_Solution
+{
+private:
+  int top;
+  int stack[30];
+  string postfix;
+
+public:
+  expression_Solution()
+  {
+    top = -1;
+    postfix = postfix_Expression;
+  }
+  int post_evaluation()
+  {
+    for (int i = 0; i < postfix.length(); i++)
+    {
+      if (postfix[i] >= '0' && postfix[i] <= '9')
+      {
+        push(postfix[i] - '0');
+      }
+      else
+      {
+        int a, b;
+        a = pop();
+        b = pop();
+        switch (postfix[i])
+        {
+        case '+':
+          push(b + a);
+          break;
+        case '-':
+          push(b - a);
+          break;
+        case '*':
+          push(b * a);
+          break;
+        case '/':
+          push(b / a);
+          break;
+        case '^':
+          push(pow(b, a));
+          break;
+        }
+      }
+    }
+    return pop();
+  }
+  void push(int ch)
+  {
+    if (top == 30 - 1) // 30 is the size of your stack
+    {
+      cout << "Stack overflow" << endl;
+      return;
+    }
+    top = top + 1;
+    stack[top] = ch;
+  }
+  char pop()
+  {
+    char c;
+    if (top == -1)
+    {
+      cout << "Stack Empty" << endl;
+      return false;
+    }
+    c = stack[top];
+    top = top - 1;
+    return c;
   }
 };
 
@@ -111,9 +186,11 @@ public:
 
 int main()
 {
-  Expr obj;
+  infix_To_Postfix obj;
   obj.inputExpression();
   obj.handling();
   obj.print();
+  expression_Solution obj2;
+  cout << "\n \t \t \t Expression Solution is = " << obj2.post_evaluation() << endl;
 }
 //********************                  Code End                    ***************************8
